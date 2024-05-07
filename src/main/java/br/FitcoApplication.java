9472e3060f27ecbbe60a306a9246992ff4525e2a
Filipe Prestes <filipe.prestes@hotmail.com>
2024-05-07 10:05:34 -0300

    aplication

---
+++ src/main/java/br/FitcoApplication.java
@@ -0,0 +1,15 @@
+package br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcoApplication.class, args);
+	/*	Runtime.Version version = Runtime.version();
+		System.out.println("Java Version:" + version);*/
+	}
+
+}
---
+++ src/test/java/br/fitco/projeto/FitcoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fitco.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
